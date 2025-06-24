bb03c94161dcd3477e4ad32d52dc9dee492983e4
AlexanderSchulga <120645846+AlexanderSchulga@users.noreply.github.com>
2025-06-24 21:39:35 +0300

    Update CucumberTestRunner.java

--- src/test/java/CucumberTestRunner.java
+++ src/test/java/CucumberTestRunner.java
@@ -1,13 +1,13 @@
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+// import org.junit.platform.suite.api.ConfigurationParameter;
+// import org.junit.platform.suite.api.IncludeEngines;
+// import org.junit.platform.suite.api.SelectClasspathResource;
+// import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+// import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps")
-public class CucumberTestRunner {
-}
+// @Suite
+// @IncludeEngines("cucumber")
+// @SelectClasspathResource("features")
+// @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps")
+// public class CucumberTestRunner {
+// }
