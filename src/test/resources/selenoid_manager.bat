cdc68595259dbdbaf99bd7d25c0d4bd30d76ef3d
AlexanderSchulga <120645846+AlexanderSchulga@users.noreply.github.com>
2025-07-07 21:19:20 +0300

    Update selenoid_manager.bat

--- src/test/resources/selenoid_manager.bat
+++ src/test/resources/selenoid_manager.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d "K:\TestDockerSelenoidJenkins\TestDockerSelenoid\src\test\resources"
+cd ".\src\test\resources"
 start "Selenoid" /wait single64 selenoid start --vnc
 timeout /t 5 /nobreak
 start "Selenoid UI" single64 selenoid-ui start
