e484a0e9ed064fcbb0d58f2f57508f2271c1f76d
Alexander <famakiller3@gmail.com>
2025-09-03 21:38:38 +0300

    page_object исправил и добавил класс

--- src/test/java/steps/CategoryPageSteps.java
+++ src/test/java/steps/CategoryPageSteps.java
@@ -2,15 +2,19 @@ package steps;
 
 import io.cucumber.java.en.Then;
 import page_objects.NavigationPageRPLSelenide;
+import page_objects.NavigationPageRplCommandName;
 
 public class CategoryPageSteps {
 
     private NavigationPageRPLSelenide navigationPageRPLSelenide = new NavigationPageRPLSelenide();
+    private NavigationPageRplCommandName navigationPageRplCommandName = new NavigationPageRplCommandName();
 
     @Then("Search and comparison")
     public void waitUntilTextRplVisible() {
 
         navigationPageRPLSelenide.waitUntilTextRplVisible();
+        navigationPageRPLSelenide.getCommandAndScrollToThePoint();
+        navigationPageRplCommandName.getCommandToThePoint();
     }
 
 }
